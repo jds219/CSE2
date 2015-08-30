bcf0a20b321e8f2f219e9a5bed636d4c38b1e377
jds219 <jds219@lehigh.edu>
2015-08-30 14:54:38 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+//Jacob Schultz
+//hw01
+//Welcome Class program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass//
+
+//define class
+public class WelcomeClass {
+    
+    //add main method
+    public static void main(String[] args){
+         System.out.println("  -------------");
+         System.out.println("  |  WELCOME  |");
+         System.out.println("  -------------");
+         System.out.println("  ^  ^  ^  ^  ^  ^");
+         System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+         System.out.println("<-J--D--S--2--1--9->");
+         System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+         System.out.println("  v  v  v  v  v  v  ");
+         System.out.println("I am from Reading, PA. I am fifth of six children. I bowled all four years of high school and during my senior year, I bowled a 700 series.");
+        
+    }
+}
